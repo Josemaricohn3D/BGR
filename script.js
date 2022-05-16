ceab5a40fd7784ffe025501aa3659bb4625995b0
José María Valenzuela Soto <josemaria.valenzuela.soto@gmail.com>
2022-05-16 21:29:58 +0200

    PRUEBA NIVEL 1 MECANICAS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let boton = document.getElementById("iniciar");
 let titulo = document.getElementById("titulo");
 let pantallaNombre = document.getElementById("NombreUsuario");
 let usuario = document.getElementById("usuario");
-const formulario = document.getElementById("espacioN")
+const formulario = document.getElementById("espacioN");
 
 function nombre(){
     titulo.style.display = "none";
@@ -10,12 +10,9 @@ function nombre(){
     pantallaNombre.style.left = "20%";
 }
 
-// function jugar (){
-//     window.location.href =("Nivel1.html")
-// }
-
 
 boton.addEventListener("click", nombre);
-formulario.addEventListener ("submit", jugar);
+// formulario.addEventListener ("submit", jugar);
 console.log ("Hola " + usuario.value)
+
 
---
+++ scriptNivel.js
@@ -0,0 +1,13 @@
+// NIVEL1 
+
+let circulo = document.getElementById("circulo1");
+const el = circulo.getBoundingClientRect();
+let circulos = document.getElementById("circulo1","circulo2", "circulo3");
+
+function desaparecer(){
+    circulos.classList.toggle("desaparecer")
+}
+
+circulos.
+console.log(el.left.toFixed())
+
